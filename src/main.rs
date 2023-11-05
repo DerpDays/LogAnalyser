9b5598bf43cd04fa62db9ce368c4689f65e68a54
sglre6355 <sglre6355@gmail.com>
2023-11-05 08:13:51 +0000

    Set up basic websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,52 @@
+use std::path::PathBuf;
+
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    response::IntoResponse,
-    Json, Router,
+    extract::ws::{WebSocketUpgrade, WebSocket},
+    routing::get,
+    response::Response,
+    Router,
 };
-use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
+mod log_parser;
+mod watch;
+
 #[tokio::main]
-async fn main() {
-    // initialize tracing
+async fn main() -> () {
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+
+    tokio::task::spawn(async move { watch::watch_logs(&PathBuf::from("../logs"), tx).await });
+    tokio::task::spawn(async move { watch::process_logs(rx).await });
+
     tracing_subscriber::fmt::init();
-
-    // build our application with a route
+    
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/", get(handler));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([100, 112, 100, 112], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    ()
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            return;
+        };
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+        if socket.send(msg).await.is_err() {
+            return;
+        }
+    }
 }
